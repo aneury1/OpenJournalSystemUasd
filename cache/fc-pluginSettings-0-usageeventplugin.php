3ad8dfecf2bdf0a361b9054f0bc39c5f4d56e9b1
aneury1 <aneury1@gmail.com>
2020-08-24 21:53:45 -0400

    first commit

---
+++ cache/fc-pluginSettings-0-usageeventplugin.php
@@ -0,0 +1,4 @@
+<?php return array (
+  'enabled' => true,
+  'uniqueSiteId' => '5f446c7a817bf',
+);
---
+++ cache/fc-pluginSettings-1-defaultthemeplugin.php
@@ -0,0 +1,7 @@
+<?php return array (
+  'baseColour' => '#268ED5',
+  'enabled' => true,
+  'showDescriptionInJournalIndex' => 'true',
+  'typography' => 'notoSans',
+  'useHomepageImageAsHeader' => 'true',
+);
